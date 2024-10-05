58ef9fa37b819147d67ac8b9106cbea11a7d7b3e
guptaji <akhilshorts87@gmail.com>
2024-10-05 15:48:03 +0530

    Conversion Of data type

--- 01_basic/03_ConversionOperation.js
+++ 01_basic/03_ConversionOperation.js
@@ -21,3 +21,11 @@ console.log(booleanIsLoggedIn);
 // "" => false
 // "akhilesh" =>true
 
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
