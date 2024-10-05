f875349ef1478eb039a58394c2f4bc7cbe44e60a
guptaji <akhilshorts87@gmail.com>
2024-10-05 15:44:26 +0530

    update

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,23 @@
+ let score = null
+
+  console.log(typeof score);
+  console.log(typeof(score));
+
+  let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" convert in 33
+// "33abc" => Nan(Not a number)
+// true => 1; false =>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0 => false
+// "" => false
+// "akhilesh" =>true
+
