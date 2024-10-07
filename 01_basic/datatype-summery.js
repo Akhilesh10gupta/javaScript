fd437ecc05467caeebb3cff32a361c003e0b56c9
guptaji <akhilshorts87@gmail.com>
2024-10-07 09:49:31 +0530

    datatype Summary

---
+++ 01_basic/datatype-summery.js
@@ -0,0 +1,38 @@
+//primitive
+
+// 7 types : string, Number,  Boolean, Null, underfined, symbool, bigint 
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+let userEmail; //it will go undefine
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 3443534666
+
+
+
+// referense (non primitive)
+
+// array, objects, functions
+
+const heros = ["spiderman", "superman", "batman"]; //array
+
+let myObj = {
+    name:"akhilesh",
+    age: 22,
+} //object
+
+const  myFunction = function(){
+    console.log("hellow world");
+    
+} //function
+
+console.log(typeof myFunction);
+
+//  https://262.ecma-international.org/5.1/#sec-11.4.3
