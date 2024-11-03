f286df97d424c52d1cfefb8cc9f87265758df416
guptaji <akhilshorts87@gmail.com>
2024-11-03 06:56:49 +0530

    learn math

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,36 @@
+const score = 400
+//console.log(score);
+
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const otherNumber = 123.8955
+
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+//+++++++++++++MATHS+++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.3));
+// console.log(Math.min(4, 3, 5, 8, 7));
+
+// console.log((Math.random()*10) +1);
+
+// random number between 1 to 6 like ludo game 
+const min = 1
+const max = 6
+
+Math.random() * (max - min + 1)
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
