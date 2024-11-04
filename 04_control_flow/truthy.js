3daa2410a8c6868be18de3ef2e1a156e05f82847
guptaji <akhilshorts87@gmail.com>
2024-11-04 16:46:29 +0530

    truthy and falsy value

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,16 @@
+//assume that string has truth value is truty value
+const userEmail = "Akhilesh@ai"
+
+if (userEmail) {
+    console.log("Got user email");
+}
+else{
+    console.log("dont have user email");
+    
+}
+
+// falsy value 
+// false, 0, -0, BigInt 0n, "", null, undefined, Nan 
+
+// truthy value
+//true, "0", 'false', " ", [], {}, function(){},
