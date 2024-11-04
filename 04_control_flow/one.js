3097f0f3313c717e56bae57c044ce6ab4e6f5b32
guptaji <akhilshorts87@gmail.com>
2024-11-04 16:26:46 +0530

    Learn about if else condition

---
+++ 04_control_flow/one.js
@@ -0,0 +1,56 @@
+//if 
+
+const isUserLogedIn = true
+const temperature = 41
+
+// if (temperature === 41){
+//     console.log("temperature is less than 50");
+    
+// }
+// else{
+//     console.log("temperature is greater than 50");
+    
+// }
+
+// const score = 200 
+// if(score>100){
+//     const power ="fly"
+//     console.log(`user power: ${power}`); 
+// }
+
+
+const balance = 1000
+// if (balance >500 ) console.log("test"),console.log("test2"); // immature code
+
+
+// if (balance < 500) {
+//     console.log("less than ");
+    
+// }
+// else if(balance <750){
+//     console.log("less than 750");
+// }
+// else if(balance <900){
+//     console.log("less than 900");
+// } 
+// else{
+//     console.log("less than 1200");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmai = true
+
+
+if (userLoggedIn && debitCard) {
+    console.log("Allow to buy course"); 
+}
+
+if (loggedInFromEmai || loggedInFromEmai) {
+    console.log("User logged in");    
+}
+
+
+// condion <, >, <=, >=, ==, !=, ===, !==
