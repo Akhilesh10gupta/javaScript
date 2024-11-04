e3774f50a5955695af040f629d270e344ba8f5aa
guptaji <akhilshorts87@gmail.com>
2024-11-04 17:24:27 +0530

    Nullish and terniary Opertor

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -14,3 +14,29 @@ else{
 
 // truthy value
 //true, "0", 'false', " ", [], {}, function(){},
+
+const emptyObj = {}
+
+if(Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+    
+}
+
+// Nullish Coalesing Operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+
+console.log(val1);
+
+
+// Terniary Operator
+
+//condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80"): console.log("More than 80");
+
