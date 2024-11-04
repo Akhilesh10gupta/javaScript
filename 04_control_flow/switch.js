d027dc3ca05c07aa3e19da5bfdaeffd39d25293e
guptaji <akhilshorts87@gmail.com>
2024-11-04 16:36:46 +0530

    switch conditon

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febury");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("defult Case match");
+        
+        break;
+}
+
+
+const month2 = "march"
+
+switch (month2) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Febury");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+
+    default:
+        console.log("defult Case match");
+        
+        break;
+}
