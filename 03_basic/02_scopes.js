7de1214f2fc72269df2a1b2c805c800234f81443
guptaji <akhilshorts87@gmail.com>
2024-11-04 06:55:24 +0530

    how scope are work and var

---
+++ 03_basic/02_scopes.js
@@ -0,0 +1,14 @@
+//{} callled scope
+
+let a = 300
+if(true){
+    let a = 10
+    const b = 20
+   // var c = 30
+   console.log("INNER", a);  
+}
+
+console.log(a);
+//console.log(b);
+//console.log(c);
+ 
