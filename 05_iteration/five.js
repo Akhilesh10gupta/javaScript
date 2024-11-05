bf526bd4d434b10a7e71d33305f5bf3746a4fbe5
guptaji <akhilshorts87@gmail.com>
2024-11-05 08:38:04 +0530

    object array using loop in more detail

---
+++ 05_iteration/five.js
@@ -0,0 +1,41 @@
+
+const coading = ["js", "ruby", "java", "python", "cpp"]
+// coading.forEach(function greet(value){
+//     console.log(value);
+    
+// });
+
+
+// coading.forEach((item) =>{
+//     console.log(item);
+    
+// })
+
+
+
+// coading.forEach((item,index,arr) =>{
+//     console.log(item,index,arr);
+    
+// })
+
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "jaya"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    }
+]
+
+myCoding.forEach((item) => {
+    
+    console.log(item.languageName );
+    
+})
