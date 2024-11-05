7adc91a799bc9d833b4fecd83d2753f060e0af2b
guptaji <akhilshorts87@gmail.com>
2024-11-05 08:16:06 +0530

    for in

---
+++ 05_iteration/four.js
@@ -0,0 +1,22 @@
+//obj
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+//for in basically use in  object
+
+for (const key in myObject) {
+   // console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+
+//array 
+const programming = ["js", "ruby", "py", "java", "cpp"]
+for (const key in programming) {
+    console.log(programming[key]);
+        
+}
