c3c8efcc6ed4c87e5582b184e3de74bf8e1df1d1
guptaji <akhilshorts87@gmail.com>
2024-11-20 10:18:12 +0530

    bmi Calulator made

--- 07_projects/2-BMICalculator/BMI.js
+++ 07_projects/2-BMICalculator/BMI.js
@@ -1,45 +1,3 @@
-# Project related to DOM
-## Project Link 
-[Click here](https://stackblitz.com/edit/dom-project-chaiaurcode?file=index.html)
-
-#  solution Code
-
-## Project 1 Backgrounf color changer
-
-```javascript 
-console.log("Akhilesh")
-
-const buttons = document.querySelectorAll('.button');
-const body = document. querySelector('body');
-
-buttons. forEach(function (button) {
-    console. log(button);
-    button. addEventListener('click', function(e){
-        console. log(e)
-        console. log(e.target)
-            if(e.target.id === 'grey'){
-                body.style.backgroundColor = e.target.id;
-            }
-            if(e.target.id === 'white'){
-                body.style.backgroundColor = e.target.id;
-            }
-            if(e.target.id === 'blue'){
-                body.style.backgroundColor = e.target.id;
-            }
-            if(e.target.id === 'yellow'){
-                body.style.backgroundColor = e.target.id;
-            }
-            if(e.target.id === 'purple'){
-                body.style.backgroundColor = e.target.id;
-            }
-        })
-    });
-
-
-```
-## Project 2 BMI
-
-```javascript
 const form = document.querySelector('form')
 
 form.addEventListener('submit',function(e){
@@ -71,14 +29,3 @@ form.addEventListener('submit',function(e){
         }
     }
 });
-
-```
-
-
-
-```html
-<h1>Akhilesh</h1>
-
-
-
-```
