2f7e4708749e445bd61bdffce39dbe69809a8120
guptaji <akhilshorts87@gmail.com>
2024-12-04 21:48:48 +0530

    learn about getter and setter

--- 10_classes_oop/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-class Teacher extends User {
-//     constructor(username, email){
-//         super(username)
-//         this.email = email
-//     }
-// }
-
-// const iphone = new Teacher("iphone", "i@phone.com")
-// console.log(iphone.createId());
